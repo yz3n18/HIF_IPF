2dc41a8b1cc8c23cc04a866f8be2870722ed1856
yz3n18 <47031705+yz3n18@users.noreply.github.com>
2022-01-07 11:39:45 +0000

    Update HIF_IPF_Figure1 & S1.R

--- HIF_IPF_Figure1 & S1.R
+++ HIF_IPF_Figure1 & S1.R
@@ -2,6 +2,11 @@ library(GEOquery)
 library(survival)
 library(GSVA)
 setwd('/Users/lefan/Rcode/IPF-singlecell/GSE70866_RAW_BAL/')
+Hypoxia_list<-list(c('NDRG1','ENO1','VEGFA','MRPS17','TPI1','CDKN3',
+                     'MIF','LDHA','ALDOA','TUBB6','PGAM1','SLC2A1','P4HA1','ACOT7','ADM'))
+Oxidative_list<-list(c('ABCC1', 'CDKN2D', 'FES', 'GCLC', 'GCLM', 'GLRX2', 'HHEX', 'IPCEF1', 
+                       'JUNB', 'LAMTOR5', 'LSP1', 'MBP', 'MGST1', 'MPO', 'NDUFA6', 'PFKP', 'PRDX1', 'PRDX2','PRDX4', 
+                       'PRNP', 'SBNO2','SCAF4', 'SOD1', 'SOD2', 'RXN1', 'TXN', 'TXNRD1'))
 
 # Figure 1A
 GSE92592<-read.table('GSE92592_gene.counts.txt',header = T,sep = '\t')
@@ -11,8 +16,35 @@ rownames(GSE92592)<-GSE92592$Group.1
 GSE92592<-GSE92592[,-1]
 GSE92592<-as.matrix(GSE92592)
 gsva_Hypoxia<-gsva(GSE92592,Hypoxia_list, mx.diff=1,kcdf= "Poisson")
+gsva_Oxidative <- gsva(GSE92592,Oxidative_list , mx.diff=1,kcdf= "Poisson")
 
-# Figure 1B/C/D
+df<-rbind(gsva_Oxidative,gsva_Hypoxia)
+df<-t(df)
+df<-as.data.frame(df)
+colnames(df)<-c('Oxidative_Stree_Score','HIF_Score')
+
+# Figure 1B
+pdf('Oxidative_Stree_HIF.pdf',width = 10,height = 10)
+par(mar=c(5,8,100,5))
+ggscatter(df, x = "Oxidative_Stree_Score", y = "HIF_Score", 
+          size = 5,
+          xlab = expression(paste('HIF',' Score')),
+          ylab = expression(paste('Oxidative Stress',' Score')),
+          add = "reg.line",  # Add regressin line
+          #cor.coef = TRUE,
+          add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+          
+          #conf.int = TRUE, # Add confidence interval
+          cor.coeff.args = list(method = "pearson", label.x =-0.07 , label.sep = "\n",
+                                label.y = 1.1),cor.coef.size = 10)+
+  annotate("text", x =-0.025, y=1.0, label = substitute(paste(italic("R"), " = 0.89")),size=10)+
+  annotate("text", x = -0.025, y=0.9, label = substitute(paste(italic("P"), " = 7.9 x ",10^-15)),size=10)+
+  theme(axis.title=element_text(size=40,face="bold"),axis.text=element_text(size=35,face="bold"),axis.title.x=element_text(face = 'bold'),
+        axis.title.y=element_text(face = 'bold'))
+dev.off()
+
+
+# Figure 1C/D
 #### download data ####
 gse70867<-getGEO('GSE70867',destdir = '.')
 rm(gse70867)
@@ -60,7 +92,6 @@ d = gse70867[[4]]
 phe_d=pData(d)
 phe_d<-phe_d[,1:17]
 raw_exprSet_d=exprs(d) 
-?cbind
 raw_exprSet_d<-na.omit(raw_exprSet_d)
 dd<-"./GPL570.soft"
 dd_nn <- grep("^[^#!^]", readLines(dd))[1] - 1
@@ -74,7 +105,6 @@ for (i in 1:54676){
 }
 pfinfo_dd<-read.csv('pfinfo_dd.csv',sep = ',')
 pfinfo_dd<-pfinfo_dd[,1:2]
-?strsplit
 raw_exprSet_d<-as.data.frame(raw_exprSet_d)
 raw_exprSet_d$ID<-rownames(raw_exprSet_d)
 raw_exprSet_d<-merge(raw_exprSet_d,pfinfo_dd,by='ID')
@@ -90,7 +120,6 @@ write.csv(raw_exprSet_d,'COPD_nor_expression.csv')
 gsva_Hypoxia <- gsva(raw_exprSet_d,Hypoxia_list , mx.diff=1)
 gsva_Hypoxia<-as.data.frame(t(gsva_Hypoxia))
 write.csv(gsva_Hypoxia,'COPD_gsva_Hypoxia.csv')
-rm(dd,dd_nn,pfinfo_dd,raw_exprSet_d)
 
 #### Probe ID transfer ####
 bb <- "./GPL14550.soft"
@@ -101,8 +130,7 @@ pfinfo_bb <- read.table(bb, sep = "\t", quote = "", header = TRUE, skip = bb_nn,
 pfinfo_bb <- pfinfo_bb[, c(1,7)]
 pfinfo_cc <- read.table(cc, sep = "\t", quote = "", header = TRUE, skip = cc_nn, fill = TRUE)
 pfinfo_cc <- pfinfo_cc[, c(1,7)]
-rm(bb,cc,bb_nn,cc_nn,pfinfo_bb,pfinfo_cc)
-?grep
+
 ## bb group
 raw_exprSet<-as.data.frame(raw_exprSet)
 raw_exprSet$ID<-rownames(raw_exprSet)
@@ -117,7 +145,6 @@ colnames(raw_exprSet_c)
 raw_exprSet_c<-merge(raw_exprSet_c,pfinfo_cc,by='ID')
 raw_exprSet_c<-raw_exprSet_c[,-1]
 raw_exprSet_c<-aggregate(x=raw_exprSet_c[,1:(ncol(raw_exprSet_c)-1)],by=list(raw_exprSet_c$GENE_SYMBOL),FUN=mean)
-rm(raw_exprSet_c)
 
 #### microarray_unique_value function ####
 microarray_unique_value<-function(x,proble_id,Func){
@@ -159,21 +186,16 @@ phe_final_HDIPF_COPD$status<-c(rep(1,82),rep(2,50),rep(3,64),rep(4,57))
 batch<-phe_final_HDIPF_COPD$status
 dim(raw_exprSet_final_numeric)
 ### remove batch
-?ComBat
+
 raw_exprSet_final_batch<- ComBat(dat=raw_exprSet_final_numeric, batch=batch)
-?ComBat
+
 raw_exprSet_final_batch<-as.matrix(raw_exprSet_final_batch)
 save(raw_exprSet_final_batch,phe_final_HDIPF,file = 'GSE70866_HDIPF_rmBatch_matrix.Rdata')
 
-Hypoxia_list<-list(c('NDRG1','ENO1','VEGFA','MRPS17','TPI1','CDKN3',
-                     'MIF','LDHA','ALDOA','TUBB6','PGAM1','SLC2A1','P4HA1','ACOT7','ADM'))
 
 gsva_Hypoxia <- gsva(raw_exprSet_final_batch,Hypoxia_list , mx.diff=1)
 gsva_Hypoxia <- gsva(Freiburg,Hypoxia_list , mx.diff=1)
 
-Oxidative_list<-list(c('ABCC1', 'CDKN2D', 'FES', 'GCLC', 'GCLM', 'GLRX2', 'HHEX', 'IPCEF1', 
-                       'JUNB', 'LAMTOR5', 'LSP1', 'MBP', 'MGST1', 'MPO', 'NDUFA6', 'PFKP', 'PRDX1', 'PRDX2','PRDX4', 
-                       'PRNP', 'SBNO2','SCAF4', 'SOD1', 'SOD2', 'RXN1', 'TXN', 'TXNRD1'))
 gsva_Hypoxia <- gsva(raw_exprSet_final_batch,Hypoxia_list , mx.diff=1)
 gsva_Oxidative <- gsva(raw_exprSet_final_batch,Oxidative_list , mx.diff=1)
 
@@ -185,7 +207,7 @@ df<-t(df)
 df<-as.data.frame(df)
 colnames(df)<-c('Oxidative_Stree_Score','HIF_Score')
 
-## plot
+## Figure 1-figure supplement 1
 pdf('Oxidative_Stree_HIF.pdf',width = 10,height = 10)
 par(mar=c(5,8,100,5))
 ggscatter(df, x = "Oxidative_Stree_Score", y = "HIF_Score", 
@@ -199,8 +221,8 @@ ggscatter(df, x = "Oxidative_Stree_Score", y = "HIF_Score",
           #conf.int = TRUE, # Add confidence interval
           cor.coeff.args = list(method = "pearson", label.x =-0.07 , label.sep = "\n",
                                 label.y = 1.1),cor.coef.size = 10)+
-  annotate("text", x =-0.025, y=1.0, label = substitute(paste(italic("R"), " = 0.89")),size=10)+
-  annotate("text", x = -0.025, y=0.9, label = substitute(paste(italic("P"), " = 7.9 x ",10^-15)),size=10)+
+  annotate("text", x =-0.025, y=1.0, label = substitute(paste(italic("R"), " = 0.48")),size=10)+
+  annotate("text", x = -0.025, y=0.9, label = substitute(paste(italic("P"), " = 1.3 x ",10^-11)),size=10)+
   theme(axis.title=element_text(size=40,face="bold"),axis.text=element_text(size=35,face="bold"),axis.title.x=element_text(face = 'bold'),
         axis.title.y=element_text(face = 'bold'))
 dev.off()
